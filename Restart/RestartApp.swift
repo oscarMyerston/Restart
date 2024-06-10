b69da879e885b5ecd32006f9ac84330f55c08899
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-10 18:23:15 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Oscar David Myerston Vega on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
