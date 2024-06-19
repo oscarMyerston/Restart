c4a1f13b8b233051c44f4782b23198df725b414d
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-18 21:54:40 -0500

    Feature: Drag gesture

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct OnboardingView: View {
     // MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffSet: CGFloat = 0
 
     // MARK: - BODY
     var body: some View {
@@ -69,7 +71,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffSet + 80)
                         Spacer()
                     }
                     // 4. CIRCLE (DRAGGABLE)
@@ -85,14 +87,29 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffSet)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffSet <= buttonWidth - 80 {
+                                        buttonOffSet = gesture.translation.width
+                                    }
+                                }
+                                .onEnded{ _ in
+                                    if buttonOffSet > buttonWidth / 2 {
+                                        buttonOffSet = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffSet = 0
+                                    }
+                                }
+
+                        )// GESTURE
                         Spacer()
                     }//: HStack
 
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             }//: VSTACK
         }//: ZSTACK
